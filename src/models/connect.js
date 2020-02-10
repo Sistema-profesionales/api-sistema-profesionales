48b4ea4ad93d0c079c65986596b38ffa098dda14
Ignacio <shoik.xtazy@gmail.com>
2020-02-10 01:25:27 -0300

    mod config connection

--- src/models/connect.js
+++ src/models/connect.js
@@ -4,16 +4,15 @@ const { Pool } = require("pg");
 
 
 const pool = new Pool({
-    // user : process.env.USER_DATABASE,
-    // host : process.env.HOST_DATABASE,
-    // database : process.env.DATABASE_NAME,
-    // password : process.env.PASSWORD_DATABASE
+    user : process.env.USER_DATABASE,
+    host : process.env.HOST_DATABASE,
+    database : process.env.DATABASE_NAME,
+    password : process.env.PASSWORD_DATABASE
 
-
-    user : 'krgwtsdd',
-    host : 'rajje.db.elephantsql.com',
-    database : 'krgwtsdd',
-    password : 'u9oI8vV2OJMdiA1ECpYdVKe3QClRXr0Y'
+    // user : 'krgwtsdd',
+    // host : 'rajje.db.elephantsql.com',
+    // database : 'krgwtsdd',
+    // password : 'u9oI8vV2OJMdiA1ECpYdVKe3QClRXr0Y'
 });
 
 
