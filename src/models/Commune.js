b41c82935e9e0490e0043aae3c0146c69cfeaea0
ignacioainol <iggnaxios@gmail.com>
2020-02-18 17:25:53 -0300

    get commune by id

--- src/models/Commune.js
+++ src/models/Commune.js
@@ -16,6 +16,24 @@ const getAll = async () => {
     }
 }
 
+const getById = async (id) => {
+    const connection = await connecting();
+
+    try {
+        const query = `SELECT * 
+                       FROM communes
+                       WHERE id = $1`;
+
+        const result = await connection.query(query, [id]);
+        return result.rows;
+    } catch (error) {
+        throw { error };
+    } finally {
+        connection.release();
+    }
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getById
 }
--- src/routes/communes.js
+++ src/routes/communes.js
@@ -2,14 +2,25 @@ const express = require('express');
 const router = express.Router();
 const communeModel = require('../models/Commune');
 
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     try {
         const communes = await communeModel.getAll();
         res.status(200).send(communes);
     } catch (error) {
         res.status(500).send(error);
     }
-})
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const idCommune = req.params.id;
+
+        const commune = await communeModel.getById(idCommune);
+        res.status(200).send(commune);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 module.exports = router;
 
