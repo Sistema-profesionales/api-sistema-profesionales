ca3d75b725d44d409dff4fb5d3096a81f74af045
ignacioainol <iggnaxios@gmail.com>
2020-03-06 13:01:06 -0300

    validation users ok :D

--- src/helpers/utils/utilitiesFuctions.js
+++ src/helpers/utils/utilitiesFuctions.js
@@ -36,8 +36,14 @@ const validator = {
     }
 }
 
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 module.exports = {
     camel,
     hasNumber,
-    validator
+    validator,
+    validateEmail
 }
--- src/helpers/validators/user.js
+++ src/helpers/validators/user.js
@@ -1,13 +1,16 @@
-const { hasNumber,validator } = require('../utils/utilitiesFuctions');
+const { hasNumber,validator, validateEmail } = require('../utils/utilitiesFuctions');
 const save = (user) => {
     const errors = {
         rut: [],
         names: [],
         lastnames: [],
-        commune_id: []
+        commune_id: [],
+        login: [],
+        phone: [],
+        email: []
     };
 
-    let { rut, names, lastnames, commune_id } = user;
+    let { rut, names, lastnames, commune_id, login, phone, email } = user;
 
     //rut validations
     if (!rut) {
@@ -57,10 +60,47 @@ const save = (user) => {
         }
     }
 
+    //login validator
+    if(!login){
+        errors.login.push('El campo login es requerido');
+    }else{
+        if(login.length < 3){
+            errors.login.push('El campo login debe contener mínimo 3 caracteres');
+        }
+
+        if(hasNumber(login)){
+            errors.login.push('El campo login no deben contener números');
+        }
+    }
+
+    //phone validation
+    if(phone){
+        if(isNaN(phone)){
+            errors.phone.push("El teléfono debe ser sólo números");
+        }
+
+        if(phone.length < 7){
+            errors.phone.push("El teléfono debe tener mínimo 7 números");
+        }
+    }
+
+
+    //email validator
+    if(!email){
+        errors.email.push("El email es requerido");
+    }else{
+        if(!validateEmail(email)){
+            errors.email.push("El email es inválido");
+        }
+    }
+
     if (errors.rut.length > 0 ||
         errors.names.length > 0 ||
         errors.lastnames.length > 0 ||
-        errors.commune_id.length > 0 ) {
+        errors.commune_id.length > 0  ||
+        errors.login.length > 0 || 
+        errors.phone.length > 0 ||
+        errors.email.length > 0 ) {
         return errors;
     } else {
         return undefined;
