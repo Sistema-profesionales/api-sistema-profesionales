1a527cb4a3fd5923b242ec2d903e80a78eaa514d
Ignacio <shoik.xtazy@gmail.com>
2020-03-11 07:05:38 -0300

    professions insert by user

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 const userModel = require('../models/User');
 const validatorUser = require('../helpers/validators/user');
 
-router.get('/', async (req,res) => {
+router.get('/', async (req, res) => {
     try {
         const users = await userModel.getAll();
 
@@ -14,12 +14,12 @@ router.get('/', async (req,res) => {
     }
 });
 
-router.get('/:id', async (req,res) => {
+router.get('/:id', async (req, res) => {
     try {
         const id = parseInt(req.params.id);
         const user = await userModel.getById(id);
 
-        if(!user) return res.status(404).send({ "user": [`El usuario con id ${id} no existe`] });
+        if (!user) return res.status(404).send({ "user": [`El usuario con id ${id} no existe`] });
 
         res.status(200).send(user);
     } catch (error) {
@@ -32,27 +32,28 @@ router.post('/', async (req, res) => {
         const { body } = req;
         const errors = validatorUser.save(body);
 
-        if(errors){
+        if (errors) {
             res.status(400).send(errors);
             return;
         }
 
         //check if user exists
         const userRut = await userModel.checkIfRutExist(body);
-        if(userRut) return res.status(409).send({"user": [`El rut ${body.rut} ya se encuentra registrado`]});
+        if (userRut) return res.status(409).send({ "user": [`El rut ${body.rut} ya se encuentra registrado`] });
 
         const userEmail = await userModel.checkIfEmailExist(body);
-        if(userEmail) return res.status(409).send({"user": [`El email ${body.email} ya se encuentra registrado`]});
+        if (userEmail) return res.status(409).send({ "user": [`El email ${body.email} ya se encuentra registrado`] });
 
         const userLogin = await userModel.checkIfLoginlExist(body);
-        if(userLogin) return res.status(409).send({"user": [`El usuario con nickname ${body.login} ya se encuentra registrado`]});
+        if (userLogin) return res.status(409).send({ "user": [`El usuario con nickname ${body.login} ya se encuentra registrado`] });
 
 
         const newUser = await userModel.save(body);
-        if(body.professions){
-            for(let i = 0; i < body.professions.length; i++){
+        if (body.professions) {
+            for (let i = 0; i < body.professions.length; i++) {
                 await userModel.insertUserProfession(newUser.id, body.professions[i].id);
             }
+            console.log(body.professions);
         }
 
         res.status(201).send(newUser);
@@ -66,7 +67,7 @@ router.delete('/:id', async (req, res) => {
         const id = parseInt(req.params.id);
         const userDeleted = await userModel.remove(id);
 
-        if(!userDeleted) return res.status(404).send({"user": [`El usuario con id ${id} no existe`]});
+        if (!userDeleted) return res.status(404).send({ "user": [`El usuario con id ${id} no existe`] });
 
         res.status(200).send(userDeleted);
     } catch (error) {
@@ -74,22 +75,22 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
-router.post('/login', async (req,res) => {
+router.post('/login', async (req, res) => {
     try {
         const { login, password } = req.body;
 
         const userLogin = await userModel.getByLogin(login);
 
-        if(userLogin){
-            if(password === userLogin.password){
+        if (userLogin) {
+            if (password === userLogin.password) {
                 delete userLogin.password;
                 res.status(200).send(userLogin);
-            }else{
-                res.status(400).send({'user': ['Usuario o contraseña incorrecto']});
+            } else {
+                res.status(400).send({ 'user': ['Usuario o contraseña incorrecto'] });
                 return;
             }
-        }else{
-            res.status(400).send({'user': ['Usuario no existe']});
+        } else {
+            res.status(400).send({ 'user': ['Usuario no existe'] });
             return;
         }
 
@@ -106,7 +107,7 @@ router.post('/login', async (req,res) => {
 //         const user = await userModel.getById(id);
 
 //         if( !user) return res.status(404).send({"user": [`El usuario con id ${id} no existe`]});
-        
+
 
 //     } catch (error) {
 //         res.status(500).send(error);
