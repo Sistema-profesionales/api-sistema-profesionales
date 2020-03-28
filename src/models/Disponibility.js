c4b0addbc23afa85ba05386207ee0d6623c13894
Ignacio <shoik.xtazy@gmail.com>
2020-03-28 14:56:16 -0300

    disponibility almost ok

--- src/models/Disponibility.js
+++ src/models/Disponibility.js
@@ -6,11 +6,19 @@ const save = async (body) => {
 
     try {
         const query = `INSERT INTO disponibility
-                      (user_id, )`
+                      (user_id, day_of_week, start_hoor, end_hour)
+                      VALUES($1, $2, $3, $4)`;
+
+        const values = [body.userId, body.dayOfWeek, body.startHour, body.endHour];
+        const result = await connection.query(query, values);
+
+        const data = result.rows[0];
+        return data ? camel(data) : null;
+
     } catch (error) {
-
+        throw { error };
     } finally {
-
+        connection.release();
     }
 }
 
